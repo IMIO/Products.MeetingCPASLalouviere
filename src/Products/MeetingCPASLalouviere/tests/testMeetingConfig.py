7c2693a6e84c9e0097ec2a269f99d8610c1e9ade
André NUYENS <andre.nuyens@imio.be>
2020-05-20 10:23:49 +0200

    test fixed in personalLabels

--- src/Products/MeetingCPASLalouviere/tests/testMeetingConfig.py
+++ src/Products/MeetingCPASLalouviere/tests/testMeetingConfig.py
@@ -22,6 +22,8 @@
 # 02110-1301, USA.
 #
 
+from imio.helpers.cache import cleanRamCacheFor
+from plone import api
 from Products.MeetingCPASLalouviere.tests.MeetingCPASLalouviereTestCase import MeetingCPASLalouviereTestCase
 from Products.MeetingCommunes.tests.testMeetingConfig import testMeetingConfig as mctmc
 
@@ -32,6 +34,17 @@ class testMeetingConfig(MeetingCPASLalouviereTestCase, mctmc):
     def test_pm_call_searchItemsToPrevalidate(self):
         '''No sense...'''
         pass
+
+    def test_pm_UpdatePersonalLabels(self):
+        """ """
+        # remove extra users from their groups to not break test
+        for extra_user_id in ['pmN1', 'pmN2', 'pmSecretaire']:
+            user = api.user.get(extra_user_id)
+            # remove from every groups, bypass Plone groups (including virtual)
+            for group_id in [user_group_id for user_group_id in user.getGroups() if '_' in user_group_id]:
+                api.group.remove_user(groupname=group_id, username=extra_user_id)
+        cleanRamCacheFor('Products.PloneMeeting.ToolPloneMeeting._users_groups_value')
+        super(testMeetingConfig, self).test_pm_UpdatePersonalLabels()
 
 
 def test_suite():
