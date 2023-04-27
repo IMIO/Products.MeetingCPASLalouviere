3fa5509012b0cd143a33af734ee13fd724119825
odelaere <odelaere@users.noreply.github.com>
2023-04-27 17:10:12 +0200

    testSetup:test_pm_InstallAvailableProfiles

--- src/Products/MeetingCPASLalouviere/profiles/zlalouvierecpas/import_data.py
+++ src/Products/MeetingCPASLalouviere/profiles/zlalouvierecpas/import_data.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from copy import deepcopy
-from Products.PloneMeeting.profiles import UserDescriptor
 from Products.MeetingCommunes.profiles.zcpas import import_data as mc_import_data
 
-from Products.MeetingCPASLalouviere.config import LLO_ITEM_CPAS_WF_VALIDATION_LEVELS
+from Products.MeetingCPASLalouviere.config import LLO_ITEM_CPAS_WF_VALIDATION_LEVELS, LLO_APPLYED_CPAS_WFA
 
 data = deepcopy(mc_import_data.data)
 
 # Meeting configurations -------------------------------------------------------
 # College communal
-data.bpMeeting.itemWFValidationLevels = deepcopy(LLO_ITEM_CPAS_WF_VALIDATION_LEVELS)
-data.bpMeeting.itemAdviceStates = ['proposed_to_president', ]
-data.bpMeeting.itemAdviceEditStates = ['proposed_to_president', 'validated']
+bpMeeting = deepcopy(mc_import_data.bpMeeting)
+bpMeeting.itemWFValidationLevels = deepcopy(LLO_ITEM_CPAS_WF_VALIDATION_LEVELS)
+bpMeeting.itemAdviceStates = ['proposed_to_president', ]
+bpMeeting.itemAdviceEditStates = ['proposed_to_president', 'validated']
+bpMeeting.workflowAdaptations = deepcopy(LLO_APPLYED_CPAS_WFA)
+bpMeeting.transitionsToConfirm = []
+bpMeeting.itemBudgetInfosStates = []
 
 # Conseil communal
-data.casMeeting = deepcopy(mc_import_data.councilMeeting)
-data.casMeeting.itemWFValidationLevels = deepcopy(LLO_ITEM_CPAS_WF_VALIDATION_LEVELS)
-data.casMeeting.itemAdviceStates = ['proposed_to_president', ]
-data.casMeeting.itemAdviceEditStates = ['proposed_to_president', 'validated']
+casMeeting = deepcopy(mc_import_data.casMeeting)
+casMeeting.itemWFValidationLevels = deepcopy(LLO_ITEM_CPAS_WF_VALIDATION_LEVELS)
+casMeeting.itemAdviceStates = ['proposed_to_president', ]
+casMeeting.itemAdviceEditStates = ['proposed_to_president', 'validated']
+casMeeting.workflowAdaptations = deepcopy(LLO_APPLYED_CPAS_WFA)
+casMeeting.transitionsToConfirm = []
+casMeeting.itemBudgetInfosStates = []
+
+data.meetingConfigs = (bpMeeting, casMeeting)
 # ------------------------------------------------------------------------------
