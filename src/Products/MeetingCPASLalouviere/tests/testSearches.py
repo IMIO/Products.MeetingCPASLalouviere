893f6f3716dd9e97e3b9447e1ca5b3fa38715710
odelaere <odelaere@users.noreply.github.com>
2023-05-10 10:21:39 +0200

    Fix test_pm_SearchItemsToValidateOfMyReviewerGroups

--- src/Products/MeetingCPASLalouviere/tests/testSearches.py
+++ src/Products/MeetingCPASLalouviere/tests/testSearches.py
@@ -24,6 +24,7 @@
 
 from Products.MeetingCPASLalouviere.tests.MeetingCPASLalouviereTestCase import MeetingCPASLalouviereTestCase
 from Products.MeetingCommunes.tests.testSearches import testSearches as mcts
+from Products.PloneMeeting.adapters import _find_nothing_query
 from Products.PloneMeeting.tests.PloneMeetingTestCase import pm_logger
 
 from Products.CMFCore.permissions import ModifyPortalContent
@@ -73,6 +74,113 @@ class testSearches(MeetingCPASLalouviereTestCase, mcts):
 
     def test_pm_SearchItemsToValidateOfHighestHierarchicLevelReturnsEveryLevels(self):
         pass
+
+    def test_pm_SearchItemsToValidateOfMyReviewerGroups(self):
+        '''Test the 'items-to-validate-of-my-reviewer-groups' adapter.
+           This should return a list of items a user could validate at any level,
+           so not only his highest hierarchic level.  This will return finally every items
+           corresponding to Plone reviewer groups the user is in.'''
+        cfg = self.meetingConfig
+        self.changeUser('admin')
+
+        # activate the 'pre_validation' wfAdaptation if it exists in current profile...
+        # if not, then reviewers must be at least 2 elements long
+        reviewers = cfg.reviewersFor()
+        if not len(reviewers) > 1:
+            self._enablePrevalidation(cfg)
+        if not len(reviewers) > 1:
+            pm_logger.info("Could not launch test 'test_pm_SearchItemsToValidateOfMyReviewerGroups' "
+                           "because we need at least 2 levels of item validation.")
+
+        # first test the generated query
+        adapter = getAdapter(cfg,
+                             ICompoundCriterionFilter,
+                             name='items-to-validate-of-my-reviewer-groups')
+        # if user si not a reviewer, we want the search to return
+        # nothing so the query uses an unknown review_state
+        itemTypeName = cfg.getItemTypeName()
+        self.assertEqual(adapter.query, _find_nothing_query(itemTypeName))
+        # for a reviewer, query is correct
+        self.changeUser('pmReviewer1')
+        # only reviewer for highest level
+        reviewers = cfg.reviewersFor()
+        self._removeUsersFromEveryGroups(['pmReviewer1'])
+        self._addPrincipalToGroup('pmReviewer1',
+                                  "{0}_{1}".format(self.developers_uid, reviewers.keys()[0]))
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        query = adapter.query
+        query['reviewProcessInfo']['query'].sort()
+        states = reviewers.values()[0]
+        self.assertEqual({'portal_type': {'query': itemTypeName},
+                          'reviewProcessInfo': {
+                          'query': sorted(['{0}__reviewprocess__{1}'.format(self.developers_uid, state)
+                                           for state in states])}},
+                         adapter.query)
+
+        # now do the query
+        # this adapter is not used by default, but is intended to be used with
+        # the "searchitemstovalidate" collection so use it with it
+        collection = cfg.searches.searches_items.searchitemstovalidate
+        patchedQuery = list(collection.query)
+        patchedQuery[0]['v'] = 'items-to-validate-of-my-reviewer-groups'
+        collection.query = patchedQuery
+
+        # create 2 items
+        self.changeUser('pmCreator1')
+        item1 = self.create('MeetingItem')
+        item2 = self.create('MeetingItem')
+        self.do(item1, 'proposeToN1')
+        self.do(item2, 'proposeToN1')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.failIf(collection.results())
+        # as first level user, he will see items
+        self.changeUser('pmN1')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 2)
+        # as second level user, he will not see items of first level also
+        self.changeUser('pmN2')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 2)
+        self.do(item1, 'proposeToN2')
+        self.do(item2, 'proposeToN2')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 2)
+        self.changeUser('pmN1')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.failIf(collection.results())
+
+        self.changeUser('pmSecretaire')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 2)
+        self.do(item1, 'proposeToSecretaire')
+        self.do(item2, 'proposeToSecretaire')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 2)
+        self.changeUser('pmN2')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.failIf(collection.results())
+        self.changeUser('pmN1')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.failIf(collection.results())
+
+        self.changeUser('pmPresident')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 2)
+        self.do(item1, 'proposeToPresident')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 2)
+        self.changeUser('pmSecretaire')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.assertEqual(collection.results().length, 1)
+        self.failUnless(collection.results()[0].UID == item2.UID())
+        self.changeUser('pmN2')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.failIf(collection.results())
+        self.changeUser('pmN1')
+        cleanRamCacheFor('Products.PloneMeeting.adapters.query_itemstovalidateofmyreviewergroups')
+        self.failIf(collection.results())
+
+
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
