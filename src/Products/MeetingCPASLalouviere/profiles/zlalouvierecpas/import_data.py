678da2cd1070afbf63ec0edd3f1901efeb9625b6
odelaere <odelaere@users.noreply.github.com>
2023-06-05 14:26:22 +0200

    fix testSetup

--- src/Products/MeetingCPASLalouviere/profiles/zlalouvierecpas/import_data.py
+++ src/Products/MeetingCPASLalouviere/profiles/zlalouvierecpas/import_data.py
@@ -15,6 +15,9 @@ bpMeeting.itemWFValidationLevels = deepcopy(LLO_ITEM_CPAS_WF_VALIDATION_LEVELS)
 bpMeeting.itemAdviceStates = ['proposed_to_president', ]
 bpMeeting.itemAdviceEditStates = ['proposed_to_president', 'validated']
 bpMeeting.workflowAdaptations = deepcopy(LLO_APPLYED_CPAS_WFA)
+bpMeeting.itemAdviceViewStates = tuple([state for state in bpMeeting.itemAdviceViewStates if state != "pre_accepted"])
+for observer in bpMeeting.powerObservers:
+    observer["item_states"] = [state for state in observer["item_states"] if state != "pre_accepted"]
 bpMeeting.transitionsToConfirm = []
 bpMeeting.itemBudgetInfosStates = []
 bpMeeting.podTemplates = deepcopy(mc_examples_fr_import_data.collegeMeeting.podTemplates)
@@ -25,6 +28,9 @@ casMeeting.itemWFValidationLevels = deepcopy(LLO_ITEM_CPAS_WF_VALIDATION_LEVELS)
 casMeeting.itemAdviceStates = ['proposed_to_president', ]
 casMeeting.itemAdviceEditStates = ['proposed_to_president', 'validated']
 casMeeting.workflowAdaptations = deepcopy(LLO_APPLYED_CPAS_WFA)
+casMeeting.itemAdviceViewStates = tuple([state for state in casMeeting.itemAdviceViewStates if state != "pre_accepted"])
+for observer in casMeeting.powerObservers:
+    observer["item_states"] = [state for state in observer["item_states"] if state != "pre_accepted"]
 casMeeting.transitionsToConfirm = []
 casMeeting.itemBudgetInfosStates = []
 casMeeting.podTemplates = deepcopy(mc_examples_fr_import_data.councilMeeting.podTemplates)
